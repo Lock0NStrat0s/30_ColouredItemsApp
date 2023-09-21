4cbd2acf14cc7d5e27e0d9faeca28864f6723bef
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-21 15:07:05 -0600

    program complete.

--- 30_ColouredItems/Program.cs
+++ 30_ColouredItems/Program.cs
@@ -4,7 +4,37 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ColouredItems<Sword> cSword = new ColouredItems<Sword>(new Sword(), ConsoleColor.Blue);
+        ColouredItems<Bow> cBow = new ColouredItems<Bow>(new Bow(), ConsoleColor.Red);
+        ColouredItems<Axe> cAxe = new ColouredItems<Axe>(new Axe(), ConsoleColor.Green);
+
+        cSword.Display();
+        cBow.Display();
+        cAxe.Display();
+
+        Console.ReadLine();
     }
 }
 
+public class ColouredItems<T>
+{
+    public T Item { get; set; }
+    public ConsoleColor Colour { get; set; }
+
+    public ColouredItems(T item, ConsoleColor clr)
+    {
+        Item = item;
+        Colour = clr;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Colour;
+        Console.WriteLine(Item.ToString());
+        Console.ForegroundColor = default;
+    }
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
